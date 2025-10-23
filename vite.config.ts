89ffecfd938a96cf8d935b04eb1303e918a0a1a2
Lucca Ballester <74988209+LuccaBallesterAlt@users.noreply.github.com>
2025-10-23 20:16:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,5 @@ export default defineConfig({
   server: {
     port: 5173,
     host: true
-  },
-  define: {
-    // Garantir que as variáveis de ambiente sejam carregadas
-    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'https://sebo-api.vercel.app')
   }
 })
